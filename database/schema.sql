5c0acf04c7b1aba411f72fcb7177aaf6ab09f5c9
F-N-Vladimir <foudjovladimir@gmail.com>
2026-05-02 00:15:38 +0400

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -5,26 +5,26 @@ USE nao_db;
 -- TABLE 1 : enfants
 -- Stocke le profil et les statistiques de chaque enfant
 
-CREATE TABLE IF NOT EXISTS enfants (
-    id               INT          AUTO_INCREMENT PRIMARY KEY,
-    nom              VARCHAR(100) NOT NULL,
+CREATE TABLE enfants (
+    id               INT          PRIMARY KEY,
+    nom              VARCHAR(100) ,
     age              INT,
     photo_path       VARCHAR(255),
-    date_creation    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-    nb_sessions      INT          DEFAULT 0,
-    score_moyen      INT          DEFAULT 0,
-    theme_difficile  VARCHAR(50)  DEFAULT 'aucun',
-    derniere_session DATETIME     DEFAULT NULL
+    date_creation    TIMESTAMP   ,
+    nb_sessions      INT         ,
+    score_moyen      INT         ,
+    theme_difficile  VARCHAR(50) ,
+    derniere_session DATETIME    
 );
 
 
 -- TABLE 2 : interactions
 -- Stocke l'historique complet de toutes les interactions
 
-CREATE TABLE IF NOT EXISTS interactions (
-    id               INT          AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE interactions (
+    id               INT       PRIMARY KEY,
     enfant_nom       VARCHAR(100),
     question         TEXT,
     reponse          TEXT,
-    date_interaction TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
+    date_interaction TIMESTAMP  
 );
